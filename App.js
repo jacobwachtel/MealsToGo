314e7f5e3a0be084459c6591ec30e6dc29f0fabc
Jacob Wachtel <jawachtel@gmail.com>
2024-02-06 14:24:46 -0500

    Changing expo-google-fonts as it errors out often when loading

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
-import {
-  useFonts as useOswald,
-  Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
-import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
-  const [oswaldLoaded] = useOswald({
+  const [fontsLoaded, fontError] = useFonts({
     Oswald_400Regular,
-  });
-
-  const [latoLoaded] = useLato({
     Lato_400Regular,
   });
+
+  if (!fontsLoaded) {
+    return <SplashScreen />;
+  }
 
   return (
     <>
